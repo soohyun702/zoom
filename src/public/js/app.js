a1062c71e255370382ab2fcdca13dc2ce263a316
cookie <tngus702@gmail.com>
2023-06-10 21:31:16 +0900

    FEAT: 카메라 선택 변경

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,10 +15,14 @@ const getCameras = async () => {
     try {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(d => d.kind === 'videoinput');
+        const currentCamera = myStream.getVideoTracks()[0];
         cameras.forEach(camera => {
             const option = document.createElement("option");
             option.value = camera.deviceId;
             option.innerText = camera.label;
+            if (currentCamera.label == camera.label) {
+                option.selected = true;
+            }
             camerasSelect.appendChild(option);
         })
     } catch (error) {
@@ -27,14 +31,27 @@ const getCameras = async () => {
 }
 
 
-const getMedia = async () => {
+const getMedia = async (deviceId) => {
+    const initialConstrains = {
+        audio: true,
+        video: { facingMode: "user" },
+    }
+    const cameraConstrains = {
+        audio: true,
+        video: {
+            deviceId: {
+                exact: deviceId,
+            }
+        },
+    }
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        })
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstrains : initialConstrains
+        )
         myFace.srcObject = myStream;
-        getCameras();
+        if (!deviceId) {
+            await getCameras();
+        }
     } catch (error) {
         console.log(error);
     }
@@ -68,5 +85,11 @@ const handleCameraClick = () => {
     }
 }
 
+
+const handleCameraChange = async () => {
+   await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
