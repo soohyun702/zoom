d669094dec8cdd813cade23d7415e84cc98dc6fa
cookie <tngus702@gmail.com>
2023-06-10 23:31:49 +0900

    FEAT: 줌 클론코딩 완료

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,10 +6,15 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
-
+let roomName;
+let myPeerConnection;
 
 const getCameras = async () => {
     try {
@@ -57,7 +62,6 @@ const getMedia = async (deviceId) => {
     }
 }
 
-getMedia();
 
 const handleMuteClick = () => {
     myStream.getAudioTracks().forEach(track => {
@@ -88,8 +92,86 @@ const handleCameraClick = () => {
 
 const handleCameraChange = async () => {
    await getMedia(camerasSelect.value);
+   if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+        .getSenders()
+        .find((sender) => sender.track.kind == "video");
+        videoSender.replaceTrack(videoTrack);
+   }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+const initCall = async () => {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+const handleWelcomeSubmit = async (event) => {
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    await initCall();
+    socket.emit("join_room", input.value);
+    roomName = input.value;
+    input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+// A 브라우저
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("sent ")
+    socket.emit("offer", offer, roomName);
+})
+
+// B 브라우저
+socket.on("offer", async (offer) => {
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+})
+
+// A 브라우저
+socket.on("answer", async (answer) => {
+    myPeerConnection.setRemoteDescription(answer);
+})
+
+// A 브라우저 & B 브라우저
+socket.on("ice", async (ice) => {
+    myPeerConnection.addIceCandidate(ice)
+})
+
+// RTC Code
+
+const makeConnection = () => {
+    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    // 기존에는 addStream 사용했으나 낡은 코드 현재는 addTrack
+    myStream
+    .getTracks()
+    .forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+
+const handleIce = async (data) => {
+    socket.emit("ice", data.candidate, roomName);
+}
+
+const handleAddStream = async (data) => {
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from 'http';
 import SocketIO from 'socket.io';
 import express from 'express';
-import { instrument } from '@socket.io/admin-ui'
 
 const app = express();
 
@@ -14,6 +13,22 @@ app.get('*', (_, res) => res.redirect('/'));
 // http server
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", socket => {
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    })
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    })
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    })
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    })
+})
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
